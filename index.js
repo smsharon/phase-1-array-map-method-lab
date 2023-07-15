4ac6acd0e8246484e55a5de8eee78617548f0989
Shallon Mokeira Said <shallonmokeira@gmail.com>
2023-07-15 03:00:12 -0400

    completed assignment

--- index.js
+++ index.js
@@ -12,5 +12,12 @@ const tutorials = [
 ];
 
 const titleCased = () => {
-  return tutorials
-}
+  return tutorials.map(title => {
+    const words = title.split(" ");
+    const capitalizedWords = words.map(word => word.charAt(0).toUpperCase() + word.slice(1));
+    return capitalizedWords.join(" ");
+  });
+};
+
+console.log(titleCased());
+
